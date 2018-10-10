66129213b65a8490597c050e274400cc8224d613
hassehulabeck <hansbudandersson@gmail.com>
2018-10-10 11:38:37 +0200

    Added color mixing on occupied stops

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Constructor för spårvagn (tram)
 function Tram(linje, position, idNummer, direction, color) {
   this.linje = linje;
   this.position = position;
@@ -26,6 +25,7 @@ function Tram(linje, position, idNummer, direction, color) {
   }
 
   // En funktion för att visa var spårvagnen befinner sig.
+  // Spårvagnen "skriker ut" sin position.
   this.shout = function() {
     if (this.direction == "Home") {
       var prevPosition = this.position + 1;
@@ -35,7 +35,28 @@ function Tram(linje, position, idNummer, direction, color) {
     }
     // Visa var spårvagnen är
     var current = document.getElementById(this.position);
-    current.style.backgroundColor = this.color;
+
+    // Kolla om det redan är någon där, i så fall kör en blandfärg.
+    var currentColor = document.getElementById(this.position).style.backgroundColor;
+    var newColor = this.color;
+
+    if (currentColor != "white") {
+      if (((currentColor == "red") && (this.color == "blue")) ||
+        ((currentColor == "blue") && (this.color == "red"))) {
+          newColor = "blueviolet";
+      }
+      if (((currentColor == "red") && (this.color == "yellow")) ||
+        ((currentColor == "yellow") && (this.color == "red"))) {
+          newColor = "orange";
+      }
+      if (((currentColor == "yellow") && (this.color == "blue")) ||
+        ((currentColor == "blue") && (this.color == "yellow"))) {
+          newColor = "lightgreen";
+      }
+
+    }
+
+    current.style.backgroundColor = newColor;
     // "Släck" den gamla
     var previous = document.getElementById(prevPosition);
     previous.style.backgroundColor = "white";
@@ -51,7 +72,7 @@ function Stop(linje, name) {
 // Skapa tre spårvagnar.
 tram1 = new Tram(13, 1, 1, "Home", "red");
 tram2 = new Tram(13, 7, 2, "Away", "yellow");
-tram3 = new Tram(13, 11, 3, "Home", "#3377d4");
+tram3 = new Tram(13, 11, 3, "Home", "blue");
 
 stopNames = [
   "Sahlgrenska",
